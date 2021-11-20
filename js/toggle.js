25a84a16e80686bcc2aaf1ba2a93ade1ba9bac81
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-11-20 18:54:41 -0300

    section banner section about completed

---
+++ js/toggle.js
@@ -0,0 +1,11 @@
+let main = document.querySelector(".main");
+let navigation = document.querySelector(".navigation");
+let topbar = document.querySelector(".topbar");
+let toggle = document.querySelector(".toggle");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  topbar.classList.toggle("active");
+  toggle.classList.toggle("active");
+  main.classList.toggle("active");
+};
